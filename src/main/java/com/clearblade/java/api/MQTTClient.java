b71d7f69230b7a41a4e60ddffaf735b3bc0b3b9b
Daniel Rivas <danielrs@users.noreply.github.com>
2020-09-04 11:47:30 -0500

    Re-subscribe to topics on MQTT connection lost

--- src/main/java/com/clearblade/java/api/MQTTClient.java
+++ src/main/java/com/clearblade/java/api/MQTTClient.java
@@ -67,7 +67,6 @@ public class MQTTClient implements MqttCallback {
 	public void connect() {
 		
 		try {
-
 			mqttClient = new MqttClient(url, clientIdentifier, memoryPersistance);
 			mqttClient.connect(opts);
 			mqttClient.setCallback(this);
@@ -113,12 +112,10 @@ public class MQTTClient implements MqttCallback {
 		}
 	}
 	
-	
 	public void publish (String topic, String message) {
 		
 		publish(topic, message.getBytes(), qualityOfService);
 	}
-	
 	
 	public void publish(String topic, byte[] payload, int qos) {
 		
@@ -139,49 +136,40 @@ public class MQTTClient implements MqttCallback {
 		}
 	}
 	
-	
 	public void subscribe(String topic, MessageCallback callback) {
-		
-		if (subscribed.contains(topic))
-			return;
-
-		else {
-			
-			subscribed.add(topic);
-		}
-		
 		subscribe(topic, qualityOfService, callback);
 	}
 	
-	
 	public void subscribe(String topic, int qos, MessageCallback callback) {
-		
 		try {
-			
 			mqttClient.subscribe(topic, qos);
-			System.out.println("Subscribed");
+			subscribed.add(topic);
 			messageReceivedCallback = callback;
-		} 
-		
+			System.out.printf("subscribed to topic: %s%n", topic);
+		}
 		catch (MqttException e) {
-			
 			e.printStackTrace();
 		}
 	}
-	
-	
-	public void unsubscribe(String topic) {
-		
-		subscribed.remove(topic);
-		
-		try {
-			
-			mqttClient.unsubscribe(topic);
-			System.out.println("Unsubscribed");
+
+	private void resubscribe() {
+		for (String topic : this.subscribed) {
+			try {
+				this.mqttClient.subscribe(topic);
+				System.out.printf("re-subscribed to topic: %s%n", topic);
+			} catch (MqttException e) {
+				e.printStackTrace();
+			}
 		}
-		
+	}
+
+	public void unsubscribe(String topic) {
+		try {
+			mqttClient.unsubscribe(topic);
+			subscribed.remove(topic);
+			System.out.printf("un-subscribed from topic: %s%n", topic);
+		}
 		catch (MqttException e) {
-			
 			e.printStackTrace();
 		}
 	}
@@ -190,7 +178,8 @@ public class MQTTClient implements MqttCallback {
 		String msg = String.format("%s. Reconnecting...", arg0.getMessage());
 		System.out.println(msg);
 		mqttClient = null;
-		connect();
+		this.connect();
+		this.resubscribe();
 	}
 
 	public void messageArrived(String topic, MqttMessage message) throws Exception {
