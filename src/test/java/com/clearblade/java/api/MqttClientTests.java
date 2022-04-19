4852a0ea328bc480c98e8ad6ccfb0889bbe834fb
skysharma <sky.sharma@gmail.com>
2022-04-19 16:12:48 -0500

    Reverted MqttClient constructor back, i.e. without specifying maxInflight.

--- src/test/java/com/clearblade/java/api/MqttClientTests.java
+++ src/test/java/com/clearblade/java/api/MqttClientTests.java
@@ -21,7 +21,7 @@ public class MqttClientTests {
 
     @BeforeEach
     void setupSpyClient() throws ClearBladeException {
-        MqttClient client = new MqttClient("url", mockAuth, "systemKey", "identifier", 0, false, 10);
+        MqttClient client = new MqttClient("url", mockAuth, "systemKey", "identifier", 0, false);
         mockAuth = mock(Auth.class);
         mockClient = mock(org.eclipse.paho.client.mqttv3.MqttClient.class);
         spyClient = spy(client);
